2caf25a4ba913de8fc4ad0b55522f1e52dca2ed5
chesswondo <chesswondo@gmail.com>
2024-05-02 17:36:47 +0300

    Добавьте файлы проекта.

---
+++ functions.h
@@ -0,0 +1,39 @@
+#pragma once
+#include<vector>
+
+void process_task1();
+
+/*long long int euler(long long int);
+int mobius(long long int);
+long long int gcd(long long int, long long int);
+long long int lcm(long long int, long long int);
+long long int lcm_set(std::vector<int>&);
+int check(std::vector<int>&);
+int evaluate(std::vector <int>& a, std::vector <int>& b, int n);
+std::vector <int> prime_factors(long long int a);
+int Lezhandro(long long int a, long long int p);
+int Jacobi(long long int a, long long int P);
+bool miillerTest(long long int d, long long int n);
+bool isPrime(long long int n, long long int k);
+long long int mod_pow(long long int base, long long int exponent, long long int modulus);
+long long int PollardRho(long long int n);
+long long int modulo(long long int a, long long int b);
+int encrypt(long long int m, long long int n);
+std::pair<int, int> eea(int a, int b);
+std::vector<long long int> decrypt(long long int c, long long int p, long long int q);
+int discreteLogarithm(int a, int b, int m);
+std::vector<int> convertToBase(int n, int b);
+std::pair<long long, long long> cipollaMult(std::pair<long long, long long> a, std::pair<long long, long long> b, long long w, long long p);
+std::pair<long long, long long> cipolla(long long int n, long long int p);
+std::pair<std::vector<int>, std::vector<int>> gen_points(int p, int a, int b);
+std::vector<int> encrypt(int k, int d, int m, std::vector<int>& arr_x, std::vector<int>& arr_y);
+
+void process_task1();
+void process_task2();
+void process_task3();
+void process_task4();
+void process_task5();
+void process_task6();
+void process_task7();
+void process_task8();
+void process_task9();*/
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <time.h>
+
+#include "functions.h"
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    int num_task;
+    while (true)
+    {
+        cout << "Enter the number of task you want to process:\n";
+        cin >> num_task;
+        cin.ignore(100, '\n');
+
+        switch (num_task)
+        {
+        case 1:
+            process_task1();
+            break;
+
+        default:
+            cout << "Incorrect number, retry\n\n";
+            break;
+        }
+
+    }
+
+    return 0;
+}
