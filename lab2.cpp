edb9183944e19930607dab88f211fdddc374be0c
chesswondo <chesswondo@gmail.com>
2024-05-08 01:58:39 +0300

    Added decryption/encryption

--- lab2.cpp
+++ lab2.cpp
@@ -58,9 +58,10 @@ class RSA
 {
 private:
     int bit_length, k;
-    long long p, q, n, lambda, d, e;
+    long long p, q, lambda, d;
 
 public:
+    long long e, n;
     RSA(int bit_length = 20, int k = 5)
     {
         this->bit_length = bit_length;
@@ -85,12 +86,47 @@ public:
         cout << "d = " << d << endl;
     }
 
-    //friend long long carmichael(long long n);
-    //friend long long mul_inv(long long a, long long b);
-    //friend long long generate_prime_number(int, int);
+    vector<long long> encrypt(vector <long long>& msg)
+    {
+        vector <long long> result;
+        for (long long s: msg)
+        {
+            long long encrypted_symbol = mod_pow(s, this->e, this->n);
+            result.push_back(encrypted_symbol);
+        }
+        return result;
+    }
+
+    vector<long long> decrypt(vector <long long>& msg)
+    {
+        vector <long long> result;
+        for (long long s : msg)
+        {
+            long long x1 = mod_pow(s, this->d % (this->p - 1), this->p);
+            long long x2 = mod_pow(s, this->d % (this->q - 1), this->q);
+            vector<long long> x = { x1, x2 };
+            vector<long long> b = { this->p, this->q };
+            long long p_inv = mul_inv(this->p, this->q);
+            long long q_inv = mul_inv(this->q, this->p);
+            result.push_back((p_inv * this->p * x2 + q_inv * this->q * x1) % this->n);
+        }
+        return result;
+    }
 };
 
 void process_task2()
 {
-    RSA rsa(23, 4);
+    RSA rsa(20, 4);
+    vector<long long> message = { 123, 456, 21, 89 };
+    cout << "Message is: ";
+    for (auto s : message) cout << s << " ";
+    cout << endl;
+    vector<long long> enc_message = rsa.encrypt(message);
+    cout << "Encrypted message is: ";
+    for (auto s : enc_message) cout << s << " ";
+    cout << endl;
+    vector<long long> dec_message = rsa.decrypt(enc_message);
+    cout << "Decrypted message is: ";
+    for (auto s : dec_message) cout << s << " ";
+    cout << "\n\n";
 }
